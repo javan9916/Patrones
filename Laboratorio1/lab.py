6418476db3cb24f1dec831031b1b18f7a3f0a9da
javan9916 <javaraya9916@gmail.com>
2020-09-19 16:13:11 -0600

    ejercicios 2,4 y 6

--- Laboratorio1/lab.py
+++ Laboratorio1/lab.py
@@ -11,8 +11,71 @@ def menu():
     print("4) Redimensionar imagen")
     print("5) Rotar imagen")
     print("6) Suavizar imagen")
-    opt = input("Opción: ")
+    print("0) Salir")
+    print("---------------------------------------------------")
 
-    print("Seleccionó "+opt)
+    while True:
+        opt = input("Opción: ")
+
+        if opt == '2':
+            region()
+            break
+        elif opt == '4':
+            redimensionar()
+            break
+        elif opt == '6':
+            suavizar()
+            break
+        elif opt == '0':
+            break
+        else:
+            print("Esa opción no existe")
+
+def region():
+    print("------ Región de una imagen ------")
+    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
+
+    ix = int(input("Inicio X: "))
+    iy = int(input("Inicio Y: "))
+    fx = int(input("Fin X: "))
+    fy = int(input("Fin Y: "))
+
+    roi = fruta[iy:fy, ix:fx]
+
+    cv2.imshow("Region", roi)
+    cv2.waitKey(0)
+
+    menu()
+
+def redimensionar():
+    print("------ Redimensionar imagen ------")
+    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
+
+    size = int(input("Tamaño a redimensionar: "))
+
+    r = size / fruta.shape[1]
+    dim = (size, int(fruta.shape[0] * r))
+    resized = cv2.resize(fruta, dim)
+
+    cv2.imshow("Redimension", resized)
+    cv2.waitKey(0)
+
+    menu()
+
+def suavizar():
+    print("------ Suavizado Gaussiano ------")
+    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
+
+    blur = int(input("Intensidad del suavizado (debe ser impar): "))
+
+    blurred = cv2.GaussianBlur(fruta, (blur, blur), 0)
+
+    cv2.imshow("Suavizado", blurred)
+    cv2.waitKey(0)
+
+    menu()
 
 menu()
