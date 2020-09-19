2da99151c4905811fa58372d7baf7ba531d98941
CACUCRC <carloscu@hotmail.es>
2020-09-19 17:09:41 -0600

    Menus y metodos modificados

--- Laboratorio1/lab.py
+++ Laboratorio1/lab.py
@@ -1,6 +1,50 @@
 import cv2
 
-def menu():
+def menuFruta():
+    print("------ Seleccione la Imagen que desea usar --------")
+    print("Seleccione su opción: ")
+    print("1) Banano")
+    print("2) Sandia")
+    print("3) Cereza")
+    print("4) Durazno")
+    print("5) Madarina")
+    print("6) Rambutan")
+    print("0) Salir")
+    print("---------------------------------------------------")
+
+    while True:
+        opt = input("Opción: ")
+
+        if opt == '1': #banano
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png"
+            menuTran(archivo)
+            break
+        elif opt == '2': #sandia
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\sandia.png"
+            menuTran(archivo)
+            break
+        elif opt == '3': #cereza
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\cereza.png"
+            menuTran(archivo)
+            break
+        elif opt == '4': #durazno
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\durazno.png"
+            menuTran(archivo)
+            break
+        elif opt == '5': #mandarina
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\mandarina.png"
+            menuTran(archivo)
+            break
+        elif opt == '6': #rambutan
+            archivo = "C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\rambutan.png"
+            menuTran(archivo)
+            break
+        elif opt == '0':
+            break
+        else:
+            print("Esa opción no existe")
+
+def menuTran(archivo):
     print("------ Transformación de imágenes con OpenCV ------")
     print("Seleccione su opción: ")
     print("1) RGB de un pixel")
@@ -16,22 +60,32 @@ def menu():
         opt = input("Opción: ")
 
         if opt == '2':
-            region()
+            region(archivo)
             break
         elif opt == '4':
-            redimensionar()
+            redimensionar(archivo)
             break
         elif opt == '6':
-            suavizar()
+            suavizar(archivo)
+            break
+        elif opt == '1':
+            getPixel(archivo)
+            break
+        elif opt == '3':
+            resizeLibre(archivo)
+            break
+        elif opt == '5':
+            rotarImagen(archivo)
             break
         elif opt == '0':
+            menuFruta(archivo)
             break
         else:
             print("Esa opción no existe")
 
-def region():
+def region(archivo):
     print("------ Región de una imagen ------")
-    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    fruta = cv2.imread(archivo)
     print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
 
     ix = int(input("Inicio X: "))
@@ -43,12 +97,12 @@ def region():
 
     cv2.imshow("Region", roi)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    menuFruta()
 
-    menu()
-
-def redimensionar():
+def redimensionar(archivo):
     print("------ Redimensionar imagen ------")
-    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    fruta = cv2.imread(archivo)
     print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
 
     size = int(input("Tamaño a redimensionar: "))
@@ -59,12 +113,12 @@ def redimensionar():
 
     cv2.imshow("Redimension", resized)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    menuFruta()
 
-    menu()
-
-def suavizar():
+def suavizar(archivo):
     print("------ Suavizado Gaussiano ------")
-    fruta = cv2.imread("C:\\Users\\Javier\\Patrones\\Laboratorio1\\frutas\\banano.png")
+    fruta = cv2.imread(archivo)
     print("Tamaño: "+ str(fruta.shape[0])+"x"+str(fruta.shape[1]))
 
     blur = int(input("Intensidad del suavizado (debe ser impar): "))
@@ -73,7 +127,53 @@ def suavizar():
 
     cv2.imshow("Suavizado", blurred)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    menuFruta()
 
-    menu()
+def getPixel(archivo):
+    print("------ RGB de Pixel ------")
 
-menu()
+    fruta = cv2.imread(archivo)
+
+    x = int(input("Posicion X: "))
+    y = int(input("Posicion Y: "))
+
+    (B, G, R) = fruta[x, y]
+
+    print("R={}, G={}, B={}".format(R, G, B))
+    menuFruta()
+
+def resizeLibre(archivo):
+    print("------ Resize de imagen libre ------")
+
+    fruta = cv2.imread(archivo)
+
+    w = int(input("Ancho: "))
+    h = int(input("Alto: "))
+
+    resized = cv2.resize(fruta, (w, h))
+
+    cv2.imshow("Resize Libre", resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    menuFruta()
+
+def rotarImagen(archivo):
+    print("------ Rotacion de la imagen ------")
+
+    fruta = cv2.imread(archivo)
+    w = fruta.shape[1]
+    h = fruta.shape[0]
+    center = (w // 2, h // 2)
+
+    angulo = int(input("Angluo de rotacion: "))
+    M = cv2.getRotationMatrix2D(center, angulo, 1.0)
+
+    rotated = cv2.warpAffine(fruta, M, (w, h))
+
+    cv2.imshow("Rotado", rotated)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    menuFruta()
+
+menuFruta()
